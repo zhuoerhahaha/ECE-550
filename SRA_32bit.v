819037c19aa46f5d8af198ba07730c35ed1a9231
Ching-Hang Hsu <30337005+jerryWTMH@users.noreply.github.com>
2021-09-21 17:41:45 -0400

    modified right_shift.v, and Added SRA-32bit.v

--- SRA_32bit.v
+++ SRA_32bit.v
@@ -1,8 +1,8 @@
-module right_shift(in, shiftamount, out);
-	input[31:0] in;
-	input[4:0] shiftamount; // 
+module SRA_32bit(A, shiftamt, shiftedA);
+	input[31:0] A;
+	input[4:0] shiftamt; // 
 	
-	output[31:0] out;
+	output[31:0] shiftedA;
 	wire[31:0] muxS0, muxS1, muxS2, muxS3, muxS4;
 	
 	// First stage using selector bit S0
@@ -11,7 +11,7 @@ module right_shift(in, shiftamount, out);
 	generate	
 		genvar i;	
 		for(i = 0; (i + 1) < 32; i = i + 1) begin: stage1
-			assign muxS0[i] = shiftamount[0]? in[i+1] : in[i];
+			assign muxS0[i] = shiftamt[0]? A[i+1] : A[i];
 		end
 	
 	// Second stage using selector bit S1
@@ -20,7 +20,7 @@ module right_shift(in, shiftamount, out);
 	generate
 		genvar j;
 		for(j = 0; (j + 2) < 32; j = j + 1) begin: stage2
-			assign muxS1[j] = shiftamount[1]? muxS0[j+2] : muxS0[j];
+			assign muxS1[j] = shiftamt[1]? muxS0[j+2] : muxS0[j];
 		end
 	endgenerate
 	
@@ -28,7 +28,7 @@ module right_shift(in, shiftamount, out);
 	generate
 		genvar k;
 		for(k = 0; (k + 4) < 32; k = k + 1) begin: stage3
-			assign muxS2[k] = shiftamount[2]? muxS1[k+4] : muxS1[k];
+			assign muxS2[k] = shiftamt[2]? muxS1[k+4] : muxS1[k];
 		end
 	endgenerate	
 	
@@ -36,7 +36,7 @@ module right_shift(in, shiftamount, out);
 	generate
 		genvar m;
 		for(m = 0; (m + 8) < 32; m = m + 1) begin: stage4
-			assign muxS3[m] = shiftamount[3]? muxS2[m+8] : muxS2[m];
+			assign muxS3[m] = shiftamt[3]? muxS2[m+8] : muxS2[m];
 		end
 	endgenerate
 	
@@ -44,10 +44,10 @@ module right_shift(in, shiftamount, out);
 	generate
 		genvar n;
 		for(n = 0; (n + 16) < 32; n = n + 1) begin: stage5
-			assign muxS4[n] = shiftamount[4]? muxS3[n+16] : muxS3[n];
+			assign muxS4[n] = shiftamt[4]? muxS3[n+16] : muxS3[n];
 		end
 	endgenerate
 	
-		assign out = muxS4;
+		assign shiftedA = muxS4;
 	
 endmodule
