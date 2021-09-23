7a8fa9628a675efc245afeb878003e2ba6c7a55b
Ching-Hang Hsu <30337005+jerryWTMH@users.noreply.github.com>
2021-09-23 18:26:24 -0400

    1

--- ALU.v
+++ ALU.v
@@ -2,12 +2,14 @@ module ALU(data_operandA, data_operandB, ctrl_ALUopcode, ctrl_shiftamt, data_res
 
    input [31:0] data_operandA, data_operandB;
    input [4:0] ctrl_ALUopcode, ctrl_shiftamt;
+	
 
    output [31:0] data_result;
    output isNotEqual, isLessThan, overflow;
 	
    // YOUR CODE HERE //
 	wire[31:0] bout, notb, add_sub_result, and_result, or_result, leftshift, rightshift, sub_result;
+	wire nothing;
 	
 	//negate operand_B
 	negateb negateb0(data_operandB, notb);
@@ -29,8 +31,9 @@ module ALU(data_operandA, data_operandB, ctrl_ALUopcode, ctrl_shiftamt, data_res
 	right_shift shift1(data_operandA, ctrl_shiftamt, rightshift);
 	
 	//subtract
-	subtract sub0(data_operandA, data_operandB, sub_result);
-	
+	CSA csa1(data_operandA, data_operandB, 5'd1, nothing, sub_result);
+
+	//
 	
 	assign data_result = ctrl_ALUopcode[2] ? (ctrl_ALUopcode[0] ? rightshift : leftshift) : (ctrl_ALUopcode[1] ? (ctrl_ALUopcode[0] ? or_result : and_result) : add_sub_result);
 	
