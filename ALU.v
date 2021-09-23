e0d577cfff7efdfcafd9ef0ae11d3eeef247d3ec
zhuoerhahaha <lizhuoer5@outlook.com>
2021-09-23 17:22:04 -0400

    add colin

--- ALU.v
+++ ALU.v
@@ -7,7 +7,7 @@ module ALU(data_operandA, data_operandB, ctrl_ALUopcode, ctrl_shiftamt, data_res
    output isNotEqual, isLessThan, overflow;
 	
    // YOUR CODE HERE //
-	wire[31:0] bout, notb, add_sub_result, and_result, or_result, leftshift, rightshift;
+	wire[31:0] bout, notb, add_sub_result, and_result, or_result, leftshift, rightshift, sub_result;
 	
 	//negate operand_B
 	negateb negateb0(data_operandB, notb);
@@ -27,6 +27,9 @@ module ALU(data_operandA, data_operandB, ctrl_ALUopcode, ctrl_shiftamt, data_res
 	//left and right shift
 	left_shift shift0(data_operandA, ctrl_shiftamt, leftshift);
 	right_shift shift1(data_operandA, ctrl_shiftamt, rightshift);
+	
+	//subtract
+	subtract sub0(data_operandA, data_operandB, sub_result);
 	
 	
 	assign data_result = ctrl_ALUopcode[2] ? (ctrl_ALUopcode[0] ? rightshift : leftshift) : (ctrl_ALUopcode[1] ? (ctrl_ALUopcode[0] ? or_result : and_result) : add_sub_result);
--- CSA.v
+++ CSA.v
@@ -3,18 +3,21 @@ module CSA(a, b, cin, ove, sum);
 	input[4:0] cin;
 	output ove;
 	output[31:0] sum;
-	wire c0, cext0, cext1, c_lastin;
+	wire c0, cext0, cext1, c_lastin0, c_lastin1, ove0, ove1;
 	wire[15:0] out0, out1;
 	
 	RCA rca0(a[15:0], b[15:0], cin[0], sum[15:0], c0);      //need 16bit RCA
-	RCA rca1(a[31:16], b[31:16], 0, out0, cext0);
-	RCA_special rca2(a[31:16], b[31:16], 1, out1, cext1, c_lastin);
+	RCA_special rca1(a[31:16], b[31:16], 0, out0, cext0, c_lastin0);
+	RCA_special rca2(a[31:16], b[31:16], 1, out1, cext1, c_lastin1);
 	
 	//2-1 mux
 	assign sum[31:16] = c0 ?  out1 : out0;  
 	
 	//overflow
-	xor(ove, c_lastin, cext1);
+	xor(ove1, c_lastin1, cext1);
+	xor(ove0, c_lastin0, cext0);
+	
+	assign ove = c0 ? ove1 : ove0;
 endmodule
 	
 	
