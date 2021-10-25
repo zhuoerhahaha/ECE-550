86c7ebf3402fc1dbbf68b0f8605d6231c339ad2f
zhuoerhahaha <lizhuoer5@outlook.com>
2021-10-24 22:08:49 -0400

    .

--- processor.v
+++ processor.v
@@ -91,24 +91,26 @@ module processor(
     input [31:0] data_readRegA, data_readRegB;
 
     /* YOUR CODE STARTS HERE */
-	 
+	
     wire DMwe, Rwe, Rwd, Rdst, ALUinB, is_Rtype, is_addi, is_sw, is_lw, is_equal, is_lessthan, ove;
     wire[31:0] sx;                                         //sign extended immediate 
+	 wire[31:0] data_readB;
     wire[31:0] aluout;                                     //output of alu
     wire[31:0] data_writeBack;
     wire[4:0] opcode;
     wire[31:0] pc_in, pc_out;
+	 wire[4:0] address_writeback, data_writeBack2, data_writeBack3, data_writeBack4;
 
     //some pc
     pc mypc1(.clock(clock), .reset(reset), .pc_in(32'd0), .pc_out(pc_out)); 
     
     assign opcode = q_imem[31:27];                           //op code
     control control_signal(opcode, DMwe, Rwe, Rwd, Rdst, ALUinB, is_Rtype, is_addi, is_sw, is_lw);                         //control signal
-    sign_extend extending(q_imem[15:0], sx);                 //sign extending
+    sign_extend extending(q_imem[17:0], sx);                 //sign extending
     
-    wire[4:0] ALUop = is_Rtype ? opcode : q_imem[6:2];       //function code
-    wire[4:0] shamt = is_Rtype ? 5'b0 : q_imem[11:7];        //shift amount
-    assign data_readB = ALUinB ? sx : data_readRegB;         //select data to feed into ALU_B
+    wire[4:0] ALUop = is_Rtype ? q_imem[6:2] : opcode;       //function code
+    wire[4:0] shamt = q_imem[11:7];                          //shift amount
+    assign data_readB = ALUinB ? data_readRegB : sx;         //select data to feed into ALU_B
     
     
     //alu module
@@ -117,21 +119,21 @@ module processor(
     //dmem
     assign data = data_readRegB;                       //only needed in sw, feed regB into data
     assign wren = DMwe;
-    assign address_dmem =  aluout[11:0]                //?
+    assign address_dmem = aluout[11:0];                //
 
 
     //writeback
     assign data_writeBack = Rwd ? q_dmem : aluout;        //select from dmem when write_back is 0; slect from alu when it's r_type or addi
     assign ctrl_writeEnable = Rwe;
-    assign address_writeback = q_imem[26:22];     //rd
+    assign address_writeback = q_imem[26:22];           //rd
     assign ctrl_writeReg = ove ? 32'd30 : address_writeback;
     assign ctrl_readRegA = q_imem[21:17];                            //rs
     assign ctrl_readRegB = q_imem[16:12];                            // 
 
     //handle the $r30 & $r0
-    wire data_writeBack2[31:0] = ALUop[0] ? 32'd2 : 32'd1;
-    wire data_writeBack3[31:0] = is_Rtype ? data_writeBack2 : 32'd3;
-    wire data_writeBack4[31:0] = ove ? data_writeBack3 : data_writeBack;
+    assign data_writeBack2 = ALUop[0] ? 32'd2 : 32'd1;
+    assign data_writeBack3 = is_Rtype ? data_writeBack2 : 32'd3;
+    assign data_writeBack4 = ove ? data_writeBack3 : data_writeBack;
     wire r0 = ctrl_writeReg[4] ? 1 : ctrl_writeReg[3] ? 1 : ctrl_writeReg[2]? 1 : ctrl_writeReg[1]? 1 : ctrl_writeReg[0]? 1 : 0;
-    data_writeReg = r0 ? data_writeBack4 : 32'd0;
+    assign data_writeReg = r0 ? data_writeBack4 : 32'd0;
 endmodule
